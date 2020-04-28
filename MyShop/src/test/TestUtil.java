2a856a9ba0069ac7800f6098490a1058bbf0274a
admin <207527106@qq.com>
2020-04-28 11:17:38 +0800

    测试
    Change-Id: I1e2ee043dd8381d6ada59997501335d275f459fb

--- MyShop/src/test/TestUtil.java
+++ MyShop/src/test/TestUtil.java
@@ -3,7 +3,11 @@ package test;
 import java.util.Date;
 
 import cn.main.utils.Contains;
-
+/**
+ * 
+ * @author sff
+ *
+ */
 public class TestUtil {
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
@@ -11,4 +15,5 @@ public class TestUtil {
 		System.out.println(Contains.getDate("2012-12-03 24:12:12"));
 		System.out.println(Contains.getDate(new Date("2012-12-03 15:02:06")));
 	}
+	
 }
